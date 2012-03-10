7bf923d225b0f3df71e24d40cbb0705004b08aa9
usuario <usuario@samsung-mini>
2012-03-10 22:10:00 +0100

    Elementos GUI de FrmJuego agregados. Pendientes de formato por HIBISCO Ver: -src/mentha/games/quiendijo/FrmJuego.java -res/layout/frmjuego.xml -AndroidManifest.xml

---
+++ src/mentha/games/quiendijo/FrmJuego.java
@@ -0,0 +1,13 @@
+package mentha.games.quiendijo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrmJuego extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frmjuego);
+	}
+}
