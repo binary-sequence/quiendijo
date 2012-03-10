cdbe059880b9112359f92ed673af019b15ab09ff
usuario <usuario@samsung-mini>
2012-03-10 22:03:12 +0100

    Cambios superfluos: -res/layout/frmusuarios.xml/etNick (Agregado inputType) -res/layout/lvusuariositem.xml/tvNick (Texto inicial para vista previa)

--- gen/mentha/games/quiendijo/R.java
+++ gen/mentha/games/quiendijo/R.java
@@ -14,21 +14,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnEstadisticas=0x7f050003;
-        public static final int btnNuevaPartida=0x7f050002;
-        public static final int btnNuevoNick=0x7f050007;
-        public static final int etNick=0x7f050006;
-        public static final int imgLogo=0x7f050004;
-        public static final int linearLayout1=0x7f050000;
-        public static final int lvUsuarios=0x7f050008;
-        public static final int tvNick=0x7f050009;
-        public static final int tvSeleccionaUsuario=0x7f050005;
-        public static final int tvTitulo=0x7f050001;
+        public static final int btnEstadisticas=0x7f05000a;
+        public static final int btnNuevaPartida=0x7f050009;
+        public static final int btnNuevoNick=0x7f05000d;
+        public static final int btnRespuesta1=0x7f050003;
+        public static final int btnRespuesta2=0x7f050004;
+        public static final int btnRespuesta3=0x7f050006;
+        public static final int btnRespuesta4=0x7f050007;
+        public static final int etNick=0x7f05000c;
+        public static final int imgLogo=0x7f050008;
+        public static final int linearLayout1=0x7f050002;
+        public static final int linearLayout2=0x7f050005;
+        public static final int lvUsuarios=0x7f05000e;
+        public static final int tvFrase=0x7f050001;
+        public static final int tvNick=0x7f05000f;
+        public static final int tvSeleccionaUsuario=0x7f05000b;
+        public static final int tvTitulo=0x7f050000;
     }
     public static final class layout {
-        public static final int frmmenu=0x7f030000;
-        public static final int frmusuarios=0x7f030001;
-        public static final int lvusuariositem=0x7f030002;
+        public static final int frmjuego=0x7f030000;
+        public static final int frmmenu=0x7f030001;
+        public static final int frmusuarios=0x7f030002;
+        public static final int lvusuariositem=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
