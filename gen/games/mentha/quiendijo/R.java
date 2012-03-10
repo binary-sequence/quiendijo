29f03962adeb1792742722122276552c1508922f
usuario <usuario@samsung-mini>
2012-03-10 18:42:43 +0100

    Elementos GUI de FrmUsuarios agregados. Pendientes de formato por HIBISCO Ver: -res/layout/frmmenu.xml (Cambiado nombre. Antes main.xml ) -src/games/mentha/quiendijo/FrmMenu.java (Cambiado layout. Antes main.xml ) -src/games/mentha/quiendijo/FrmUsuarios.java -AndroidManifest.xml -res/layout/frmusuarios.xml -res/layout/lvusuariositem.xml (Elemento para frmusuarios.xml) -res/values-es/strings.xml -res/values/strings.xml

--- gen/games/mentha/quiendijo/R.java
+++ gen/games/mentha/quiendijo/R.java
@@ -16,17 +16,27 @@ public final class R {
     public static final class id {
         public static final int btnEstadisticas=0x7f050003;
         public static final int btnNuevaPartida=0x7f050002;
+        public static final int btnNuevoNick=0x7f050007;
+        public static final int etNick=0x7f050006;
         public static final int imgLogo=0x7f050004;
         public static final int linearLayout1=0x7f050000;
+        public static final int lvUsuarios=0x7f050008;
+        public static final int tvNick=0x7f050009;
+        public static final int tvSeleccionaUsuario=0x7f050005;
         public static final int tvTitulo=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int frmmenu=0x7f030000;
+        public static final int frmusuarios=0x7f030001;
+        public static final int lvusuariositem=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int btnEstadisticas=0x7f040003;
         public static final int btnNuevaPartida=0x7f040002;
+        public static final int btnNuevoNick=0x7f040006;
+        public static final int etNick=0x7f040005;
+        public static final int tvSeleccionaUsuario=0x7f040004;
         public static final int tvTitulo=0x7f040001;
     }
 }
---
+++ src/games/mentha/quiendijo/FrmUsuarios.java
@@ -0,0 +1,13 @@
+package games.mentha.quiendijo;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class FrmUsuarios extends ListActivity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frmusuarios);
+	}
+}
