d60bc7fe102e615c1a09590193244888596ab42e
sergio <sergio@mini-blanco.local.lan>
2012-03-13 16:10:50 +0100

    Agregada clase BtnRespuesta_Click. Ver: -src/mentha/games/quiendijo/BtnRespuesta_Click.java -src/mentha/games/quiendijo/FrmJuego.java

---
+++ src/mentha/games/quiendijo/BtnRespuesta_Click.java
@@ -0,0 +1,19 @@
+package mentha.games.quiendijo;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class BtnRespuesta_Click implements OnClickListener {
+
+	FrmJuego frmJuego;
+
+	public BtnRespuesta_Click(FrmJuego frmJuego) {
+		this.frmJuego = frmJuego;
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/mentha/games/quiendijo/FrmJuego.java
+++ src/mentha/games/quiendijo/FrmJuego.java
@@ -2,6 +2,7 @@ package mentha.games.quiendijo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FrmJuego extends Activity {
 	/** Called when the activity is first created. */
@@ -9,5 +10,14 @@ public class FrmJuego extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.frmjuego);
+
+		Button[] btnRespuesta = new Button[4];
+		btnRespuesta[0] = (Button) this.findViewById(R.id.btnRespuesta1);
+		btnRespuesta[1] = (Button) this.findViewById(R.id.btnRespuesta2);
+		btnRespuesta[2] = (Button) this.findViewById(R.id.btnRespuesta3);
+		btnRespuesta[3] = (Button) this.findViewById(R.id.btnRespuesta4);
+		for (int i = 0; i < 4; i++) {
+			btnRespuesta[i].setOnClickListener(new BtnRespuesta_Click(this));
+		}
 	}
 }
