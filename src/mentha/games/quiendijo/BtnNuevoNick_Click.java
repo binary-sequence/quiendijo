48f40dc769b7f829d1bb8d69984afee903ce9cbd
sergio <sergio@mini-blanco.local.lan>
2012-03-13 15:47:32 +0100

    Agregada clase BtnNuevoNick_Click. Ver: -src/mentha/games/quiendijo/BtnNuevoNick_Click.java -src/mentha/games/quiendijo/FrmUsuarios.java

---
+++ src/mentha/games/quiendijo/BtnNuevoNick_Click.java
@@ -0,0 +1,19 @@
+package mentha.games.quiendijo;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class BtnNuevoNick_Click implements OnClickListener {
+
+	FrmUsuarios frmUsuarios;
+
+	public BtnNuevoNick_Click(FrmUsuarios frmUsuarios) {
+		this.frmUsuarios = frmUsuarios;
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/mentha/games/quiendijo/FrmUsuarios.java
+++ src/mentha/games/quiendijo/FrmUsuarios.java
@@ -3,6 +3,7 @@ package mentha.games.quiendijo;
 import mentha.games.quiendijo.R;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FrmUsuarios extends ListActivity {
 	/** Called when the activity is first created. */
@@ -10,5 +11,8 @@ public class FrmUsuarios extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.frmusuarios);
+
+		Button btnNuevoNick = (Button) this.findViewById(R.id.btnNuevoNick);
+		btnNuevoNick.setOnClickListener(new BtnNuevoNick_Click(this));
 	}
 }
