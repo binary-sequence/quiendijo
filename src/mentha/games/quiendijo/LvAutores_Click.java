ee04804e6505631ddfdbfc1def563ea40a3ad69c
sergio <sergio@localhost>
2012-03-16 16:20:25 +0100

    Agregada clase LvAutores_Click. Ver: -src/mentha/games/quiendijo/LvAutores_Click.java -src/mentha/games/quiendijo/TabAutores.java

---
+++ src/mentha/games/quiendijo/LvAutores_Click.java
@@ -0,0 +1,20 @@
+package mentha.games.quiendijo;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class LvAutores_Click implements OnItemClickListener {
+	TabAutores tabAutores;
+
+	public LvAutores_Click(TabAutores tabAutores) {
+		this.tabAutores = tabAutores;
+	}
+
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/mentha/games/quiendijo/TabAutores.java
+++ src/mentha/games/quiendijo/TabAutores.java
@@ -2,6 +2,7 @@ package mentha.games.quiendijo;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class TabAutores extends ListActivity {
 	/** Called when the activity is first created. */
@@ -9,5 +10,8 @@ public class TabAutores extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tabautores);
+
+		ListView lvAutores = getListView();
+		lvAutores.setOnItemClickListener(new LvAutores_Click(this));
 	}
 }
